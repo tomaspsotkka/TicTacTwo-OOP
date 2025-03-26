31c520922b20ce2521e972a1f3f24e806d66b1af
Tomas Psotka <psotka439@gmail.com>
2025-03-26 23:49:40 +0100

    Added some more functionality.
    - checking for winner
    - checking for draw
    - little bit of cell styling

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ class Game {
         this.statusElement = document.getElementById('status');
         this.board.showBoard();
         this.updateStatus();
+        this.restartButton = document.getElementById('restartBtn');
+        this.restartButton.addEventListener('click', () => this.restart());
     }
 
     handleCellClick(index) {
-        console.log(`Cell ${index} clicked`);
+        console.log(`Cell ${index} clicked`); //just for checking if cells clicks work 
 
         if (this.board.cells[index]) {
             return;
@@ -16,8 +18,20 @@ class Game {
 
         this.board.cells[index] = this.currentPlayer; //update state
 
-        const cellElement = this.board.boardElement.querySelector(`[data-index="${index}"]`); 
+        const cellElement = this.board.boardElement.querySelector(`[data-index="${index}"]`);
         cellElement.textContent = this.currentPlayer; //update UI
+        cellElement.classList.add('taken');
+
+        const winner = this.checkWinner();
+        if (winner) {
+            this.statusElement.textContent = `Player ${winner} wins!`;
+            return;
+        }
+
+        if (!this.board.cells.includes(null)) {
+            this.statusElement.textContent = `It's a draw!`;
+            return;
+        }
 
         this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
         this.updateStatus();
@@ -26,16 +40,44 @@ class Game {
     updateStatus() {
         this.statusElement.textContent = `Player ${this.currentPlayer}'s Turn`;
     }
+
+    checkWinner() {
+        const winCombos = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], //rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], //columns
+            [0, 4, 8], [2, 4, 6] //diagonals
+        ]
+
+        for (let combo of winCombos) {
+            const [a, b, c] = combo;
+            const cellA = this.board.cells[a];
+            const cellB = this.board.cells[b];
+            const cellC = this.board.cells[c];
+
+            if (cellA && cellA === cellB && cellB === cellC) {
+                return cellA;
+            }
+        }
+
+        return null;
+    }
+
+    restart() {
+        this.board.cells = Array(9).fill(null); //reset logic
+        this.board.showBoard(); //reload UI
+        this.currentPlayer = 'X'; //reset player back to player X
+        this.updateStatus(); //reset message
+    }
 }
 
 class Board {
-    constructor(onCellClick){
+    constructor(onCellClick) {
         this.boardElement = document.getElementById('board');
         this.cells = Array(9).fill(null);
         this.onCellClick = onCellClick;
     }
 
-    showBoard(){
+    showBoard() {
         this.boardElement.innerHTML = '';
 
         this.cells.forEach((_, index) => {
